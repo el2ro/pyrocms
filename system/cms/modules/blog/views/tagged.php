d1e360c618571cd202618d4d0df33c658439b448
Sami Torola <sami.torola@gmail.com>
2011-12-20 18:17:16 +0200

    Small taggged fix

--- system/cms/modules/blog/views/tagged.php
+++ system/cms/modules/blog/views/tagged.php
@@ -4,7 +4,7 @@
 	<div class="blog_post">
 		<!-- Post heading -->
 		<div class="post_heading">
-			<h2><?php echo  anchor(get_post_url($blog->id, $post->slug, $post->created_on, $post->category_id, $post->category_slug), $post->title); ?></h2>
+			<h2><?php echo  anchor(get_post_url($post->id, $post->slug, $post->created_on, $post->category_id, $post->category_slug), $post->title); ?></h2>
 			<p class="post_date"><?php echo lang('blog_posted_label');?>: <?php echo format_date($post->created_on); ?></p>
 			<?php if($post->category_slug): ?>
 			<p class="post_category">
